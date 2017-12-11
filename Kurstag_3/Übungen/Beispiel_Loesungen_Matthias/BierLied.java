ed5313e4ba9db6eda6ff5dfbb42ac389d19224ac
u206981 <matthias.kaufmann@sbb.ch>
2017-12-11 20:47:41 +0100

    Musterloesungen Bierlied

--- Kurstag_3/Übungen/Beispiel_Loesungen_Matthias/BierLied.java
+++ Kurstag_3/Übungen/Beispiel_Loesungen_Matthias/BierLied.java
@@ -1,15 +1,16 @@
-public class BierLied1 {
+public class BierLied {
     public static void main(String[] args) {
         int bierAnzahl = 99;
         String wort = "Flaschen";
-        while (bierAnzahl > 0) {
+        while (bierAnzahl > 0) { // hier muss man NICHT >= schreiben, weil Spezialfall bierAnzahl==0 schon unten
+                                 // mit "Kein Bier im Kühlschrank" abgehandelt wird.
             System.out.println(bierAnzahl + " " + wort + " Bier im Kühlschrank");
             System.out.println(bierAnzahl + " " + wort + " Bier.");
             System.out.println("Nimm eins raus.");
             System.out.println("Reich es herum.");
             bierAnzahl = bierAnzahl - 1;
-            if (bierAnzahl == 1) {// if-Bedingung hierher verschoben.
-                wort = "Flasche"; // Singular, wie in EINE Flasche.
+            if (bierAnzahl == 1) { // Bedingung hierher verschoben, damit schon bei der naechsten
+                wort = "Flasche";  // Ausgabe (Zeile 16) 'Flasche' verwendet wird, wenn bierAnzahl 1 ist.
             }
             if (bierAnzahl > 0) {
                 System.out.println(bierAnzahl + " " + wort + " Bier im Kühlschrank");
@@ -19,4 +20,5 @@ public class BierLied1 {
         }
     }
 }
+
 
--- Kurstag_3/Übungen/Beispiel_Loesungen_Matthias/BierLied2.java
+++ Kurstag_3/Übungen/Beispiel_Loesungen_Matthias/BierLied2.java
@@ -1,13 +1,18 @@
+/**
+ * Einfachere Loesung, allerdings nicht dieselbe Ausgabe wie Original:
+ * "99 Flaschen Bier" wird im Original nur einmal ausgegeben, hier zweimal.
+ */
 public class BierLied2 {
     public static void main(String[] args) {
         int bierAnzahl = 99;
-        String wort = "Flaschen";
         while (bierAnzahl > 0) {
+            String wort = "Flaschen";
             if (bierAnzahl == 1) {
                 wort = "Flasche";
             }
             System.out.println(bierAnzahl + " " + wort + " Bier im Kühlschrank");
             System.out.println(bierAnzahl + " " + wort + " Bier im Kühlschrank");
+            System.out.println(bierAnzahl + " " + wort + " Bier.");
             System.out.println("Nimm eins raus.");
             System.out.println("Reich es herum.");
             bierAnzahl = bierAnzahl - 1;
