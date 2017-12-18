9e29a4afc1cd004bed1f5d11ae3046bec12ae988
taaolno1 <noel.oliveira@swisscom.com>
2017-12-18 16:10:43 +0100

    sudoku

--- Kurstag_5/Übungen/Sudoku/Sudoku.java
+++ Kurstag_5/Übungen/Sudoku/Sudoku.java
@@ -1,5 +1,7 @@
 public class Sudoku {
 
+    //Mit dieser Methode können Zahlen gesetzt werden
+    //Es wird überprüft, ob in diesem Feld schon ein Wert eingetragen ist
     public static void setNumbers(int x, int y, int number) {
         if (sudoku[x][y] == null) {
             sudoku[x][y] = number;
@@ -9,7 +11,9 @@ public class Sudoku {
         }
     }
 
-    public void evalutaion(){
+    //Daten werden auf die gesamte Spalte und Zeile überprüft
+    //Ebenfalls wird überprüft, ob alle Felder abgefüllt sind
+    public static void evaluation(){
         for(x=0; x<8; x++) {
             for(y=0; y<8; y++){
                 if (sudoku[x][y] == null) {
@@ -17,8 +21,6 @@ public class Sudoku {
                 }
             }
         }
-
-
 
         for(x=0; x<8; x++){
             int c = 0;
@@ -41,7 +43,7 @@ public class Sudoku {
         }
     }
 
-
+    //setzen der ersten Zahlen, bevor das Sudoku beginnt
     public static void seDefaultNumbers(int x, int y, int defnumber) {
         sudoku[x][y] = defnumber;
     }
@@ -49,11 +51,17 @@ public class Sudoku {
 
 
     public static void main(String[] args) {
-        int[][] sudoku = new matchfield[8][8];
+        //generiert ein zwei-dimensionales Array
+        int[][] sudoku = new int[8][8];
 
+        //setzt die ersten Zahlen, die schon zu beginn des Spieles eingetragen sind
         setDefaultNumbers(2, 5, 9);
 
+        //Zahlen im Sudoku setzten
         setNumbers(1, 3, 5);
         setNumbers(2, 4, 9);
+
+        //überprüfung
+        evaluation();
     }
 }
