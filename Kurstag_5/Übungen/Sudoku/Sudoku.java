29d54208b06e625e84d3a2ef0a5c24443c6b5d60
taaolno1 <noel.oliveira@swisscom.com>
2017-12-18 16:35:40 +0100

    sudoku

--- Kurstag_5/Übungen/Sudoku/Sudoku.java
+++ Kurstag_5/Übungen/Sudoku/Sudoku.java
@@ -1,10 +1,12 @@
 public class Sudoku {
 
+    public int[][] array;
+
     //Mit dieser Methode können Zahlen gesetzt werden
     //Es wird überprüft, ob in diesem Feld schon ein Wert eingetragen ist
-    public static void setNumbers(int x, int y, int number) {
-        if (sudoku[x][y] == null) {
-            sudoku[x][y] = number;
+    public void setNumbers(int x, int y, int number) {
+        if (array[x][y] == null) {
+            array[x][y] = number;
         } else {
             System.out.println("ERROR: Field already occupied");
             System.out.println("Please enter new coordinates");
@@ -13,29 +15,29 @@ public class Sudoku {
 
     //Daten werden auf die gesamte Spalte und Zeile überprüft
     //Ebenfalls wird überprüft, ob alle Felder abgefüllt sind
-    public static void evaluation(){
-        for(x=0; x<8; x++) {
-            for(y=0; y<8; y++){
-                if (sudoku[x][y] == null) {
+    public  void evaluation(){
+        for(int x=0; x<8; x++) {
+            for(int y=0; y<8; y++){
+                if (array[x][y] == null) {
                     System.out.println("ERROR: Please fill out whole Sudoku");
                 }
             }
         }
 
-        for(x=0; x<8; x++){
+        for(int x=0; x<8; x++){
             int c = 0;
             for(int i = 0; i<8; i++) {
-                if (sudoku[x][i] == sudoku[x][c]) {
+                if (array[x][i] == array[x][c]) {
                     System.out.println("ERROR: Sudoku wrong");
                 }
                 c++;
             }
         }
 
-        for(y=0; y<8; y++){
+        for(int y=0; y<8; y++){
             int b = 0;
             for(int i = 0; i<8; i++) {
-                if (sudoku[i][y] == sudoku[b][y]) {
+                if (array[i][y] == array[b][y]) {
                     System.out.println("ERROR: Sudoku wrong");
                 }
                 b++;
@@ -44,24 +46,31 @@ public class Sudoku {
     }
 
     //setzen der ersten Zahlen, bevor das Sudoku beginnt
-    public static void seDefaultNumbers(int x, int y, int defnumber) {
-        sudoku[x][y] = defnumber;
+    public void setDefaultNumbers(int x, int y, int defnumber) {
+        array[x][y] = defnumber;
     }
 
+    public void setSudoku() {
+        int[][] sudoku = new int[8][8];
+        this.array = sudoku;
+    }
 
 
     public static void main(String[] args) {
         //generiert ein zwei-dimensionales Array
-        int[][] sudoku = new int[8][8];
+        //int[][] sudoku = new int[8][8];
 
         //setzt die ersten Zahlen, die schon zu beginn des Spieles eingetragen sind
-        setDefaultNumbers(2, 5, 9);
+        Sudoku defaultNumbers = new Sudoku();
+        defaultNumbers.setDefaultNumbers(2, 5, 9);
 
         //Zahlen im Sudoku setzten
-        setNumbers(1, 3, 5);
-        setNumbers(2, 4, 9);
+        Sudoku setnumbers = new Sudoku();
+        setnumbers.setNumbers(1, 3, 5);
+        setnumbers.setNumbers(2, 4, 9);
 
         //überprüfung
-        evaluation();
+        Sudoku evaluation = new Sudoku();
+        evaluation.evaluation();
     }
 }
