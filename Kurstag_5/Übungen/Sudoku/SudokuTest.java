d8a73aaa91ab99a39dd230b5168dba231f014101
ue82380 <ue82380@sbb.ch>
2017-12-18 15:42:10 +0100

    sudoku...

---
+++ Kurstag_5/Übungen/Sudoku/SudokuTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2017.
+ */
+
+package Sudoku;
+
+public class SudokuTest {
+    public int getZahl() {
+        return zahl;
+    }
+
+    public void setZahl(int zahl) {
+        this.zahl = zahl;
+    }
+
+
+
+    int zahl;
+
+}
