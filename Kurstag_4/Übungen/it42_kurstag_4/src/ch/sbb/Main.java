358e704c5e06bc83926b4a9244dc0e20530bc1e5
ch.grossmann <ch.grossmann@christophs-mbp.fritz.box>
2017-12-28 22:07:17 +0100

    Aktualisiert

---
+++ Kurstag_4/Übungen/it42_kurstag_4/src/ch/sbb/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2017.
+ */
+
+package ch.sbb;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        new BananenRunner().startApplication();
+    }
+}
