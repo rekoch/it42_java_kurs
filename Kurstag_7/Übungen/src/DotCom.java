ba6c6cc86729f63a88d441ead809fcb5e5e39bfb
raffael <ue66408@K27244.sbbintra.ch>
2018-02-02 14:07:34 +0100

    Commiting excercise

---
+++ Kurstag_7/Übungen/src/DotCom.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
+ */
+
+public class DotCom {
+    private int [] zellorte;
+    private int anzahltreffer;
+
+
+    public DotCom(){
+        this.zellorte = zellorte;
+        this.anzahltreffer = anzahltreffer;
+    }
+
+
+    void setZellorte(int[] orte){
+        this.zellorte = orte;
+    }
+
+
+    public void Spielstart()
+    {
+        System.out.println("Geben sie eine Zahl ein");
+    }
+}
---
+++ Kurstag_7/Übungen/src/Spiel.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
+ */
+
+public class Spiel {
+    public static void main(String[] args){
+        DotCom DotCom = new DotCom();
+    }
+}
