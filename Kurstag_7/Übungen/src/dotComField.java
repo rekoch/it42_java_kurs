76731c0b0f67edfecce8172b9d025119d99a561d
ue82371 <florian.borter@sbb.ch>
2018-02-02 15:45:51 +0100

    dotComVersenken -> 3er-Schiffe werden zufällig Platziert.

--- Kurstag_7/Übungen/src/dotComField.java
+++ Kurstag_7/Übungen/src/dotComField.java
@@ -6,35 +6,85 @@ import java.util.Random;
 
 public class dotComField {
     Random random = new Random();
-    private dotComCell[][] hiddenMap = new dotComCell[7][7];
-    private dotComCell[][] map = new dotComCell[7][7];
+    private dotComCell[][] hiddenMap = new dotComCell[9][9];
+    private dotComCell[][] map = new dotComCell[9][9];
 
     public dotComField(){
-       initField();
+       initMap();
+       generateMap();
     }
 
-    private void initField(){
-        for(int i = 0; i < 7; i++){
-            for(int k = 0; k < 7; k++){
-                hiddenMap[i][k] = new dotComCell();
-                hiddenMap[i][k].setCellValue(".");
-                map[i][k] = new dotComCell();
-                map[i][k].setCellValue(".");
+    private void initMap(){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                hiddenMap[i][j] = new dotComCell();
+                hiddenMap[i][j].setCellValue("-");
+                map[i][j] = new dotComCell();
+                map[i][j].setCellValue("-");
             }
         }
     }
 
-    private void generateField(){
+    private void generateMap(){
 
-        int x = generateRandom();
-        int y = generateRandom();
+        int x;
+        int y;
         int countDotComs = 0;
+        int view;
 
-        hiddenMap[x][y].setCellValue("X");
+        while (countDotComs < 3) {
+            view = generateRandom(2, 0);
+
+            if (view == 0){ //bei 0 ist die Ausrichtung horizontal -> sonst ist sie vertikal ^
+                x = generateRandom(5, 1);
+                y = generateRandom(7, 1);
+                if (hiddenMap[x-1][y].getCellValue().equals("-") && hiddenMap[x][y].getCellValue().equals("-") && hiddenMap[x+1][y].getCellValue().equals("-") && hiddenMap[x+2][y].getCellValue().equals("-") && hiddenMap[x+3][y].getCellValue().equals("-") &&
+                    hiddenMap[x-1][y-1].getCellValue().equals("-") && hiddenMap[x][y-1].getCellValue().equals("-") && hiddenMap[x+1][y-1].getCellValue().equals("-") && hiddenMap[x+2][y-1].getCellValue().equals("-") && hiddenMap[x+3][y-1].getCellValue().equals("-") &&
+                    hiddenMap[x-1][y+1].getCellValue().equals("-") && hiddenMap[x][y+1].getCellValue().equals("-") && hiddenMap[x+1][y+1].getCellValue().equals("-") && hiddenMap[x+2][y+1].getCellValue().equals("-") && hiddenMap[x+3][y+1].getCellValue().equals("-")
+                ){
+
+                    hiddenMap[x][y].setCellValue("X");
+                    hiddenMap[x + 1][y].setCellValue("X");
+                    hiddenMap[x + 2][y].setCellValue("X");
+                    countDotComs++;
+                }
+            } else{
+                x = generateRandom(7, 1);
+                y = generateRandom(5, 1);
+                if (hiddenMap[x][y-1].getCellValue().equals("-") && hiddenMap[x][y].getCellValue().equals("-") && hiddenMap[x][y+1].getCellValue().equals("-") && hiddenMap[x][y+2].getCellValue().equals("-") && hiddenMap[x][y+3].getCellValue().equals("-") &&
+                    hiddenMap[x-1][y-1].getCellValue().equals("-") && hiddenMap[x-1][y].getCellValue().equals("-") && hiddenMap[x-1][y+1].getCellValue().equals("-") && hiddenMap[x-1][y+2].getCellValue().equals("-") && hiddenMap[x-1][y+3].getCellValue().equals("-") &&
+                    hiddenMap[x+1][y-1].getCellValue().equals("-") && hiddenMap[x+1][y].getCellValue().equals("-") && hiddenMap[x+1][y+1].getCellValue().equals("-") && hiddenMap[x+1][y+2].getCellValue().equals("-") && hiddenMap[x+1][y+3].getCellValue().equals("-")
+                ){
+                    hiddenMap[x][y].setCellValue("X");
+                    hiddenMap[x][y + 1].setCellValue("X");
+                    hiddenMap[x][y + 2].setCellValue("X");
+                    countDotComs++;
+                }
+            }
+        }
+
+        showMap();
+
     }
 
-    private int generateRandom(){
-        return random.nextInt(8);
+    private int generateRandom(int bound, int offset){
+        return random.nextInt(bound) + offset;
     }
 
+    private void showMap(){
+        map = hiddenMap.clone();
+        /*for(int i = 1; i < 8; i++){
+            for(int j = 1; j < 8; j++){
+                System.out.print(map[i][j].getCellValue() + "\t");
+            }
+            System.out.println("\n");
+        }*/
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                //System.out.print(map[i][j].getCellValue() + "\t");
+                System.out.print(hiddenMap[i][j].getCellValue() + "\t");
+            }
+            System.out.println("\n");
+        }
+    }
 }
