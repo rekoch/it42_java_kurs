79a6a4e55037c52f5b960bd7acb5ecbf24549032
raffael <ue66408@K27244.sbbintra.ch>
2018-02-09 12:27:47 +0100

    Commiting excercise

--- Kurstag_7/Übungen/src/DotCom.java
+++ Kurstag_7/Übungen/src/DotCom.java
@@ -2,9 +2,13 @@
  * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
  */
 
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DotCom {
     private int [] zellorte;
-    private int anzahltreffer;
+    private int anzahltreffer = 0;
 
 
     public DotCom(){
@@ -12,14 +16,35 @@ public class DotCom {
         this.anzahltreffer = anzahltreffer;
     }
 
+    public String prüfDich(String tipp) {
 
-    void setZellorte(int[] orte){
-        this.zellorte = orte;
+        int tipp_int = Integer.parseInt(tipp);
+        String return_string = "";
+
+        for (int i = 0; i < zellorte.length; ++i)
+        {
+            List<int[]> zellorte_list = Arrays.asList(zellorte);  //konvertiert zellorte zu einer "List", damit compare funktioniert
+
+            boolean treffer = zellorte_list.contains(tipp_int);
+
+            if (treffer){
+                anzahltreffer++;
+                if (anzahltreffer <= 3){
+                    return_string = "Versenkt";
+                }
+                else{
+                    return_string  = "Treffer";
+                }
+            }
+            else{
+                return_string = "Vorbei";
+            }
+        }
+        return return_string;
     }
 
+    public void setZellorte(int[] orte){
 
-    public void Spielstart()
-    {
-        System.out.println("Geben sie eine Zahl ein");
+        this.zellorte = orte;
     }
 }
--- Kurstag_7/Übungen/src/Spiel.java
+++ Kurstag_7/Übungen/src/Spiel.java
@@ -2,8 +2,18 @@
  * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
  */
 
+
+import java.util.Scanner;
+
 public class Spiel {
     public static void main(String[] args){
-        DotCom DotCom = new DotCom();
+
+        System.out.println("Geben sie eine Zahl ein");
+
+        Scanner sc = new Scanner(System.in);
+        String tipp = sc.next();
+
+        DotCom dotCom = new DotCom();
+        dotCom.prüfDich(tipp);
     }
 }
