474e65cffe6cfdcf80c0b8be7d3b4e9dfd3803a1
u206981 <matthias.kaufmann@sbb.ch>
2018-05-24 22:31:16 +0200

    Uebung Lektion 14

---
+++ Kurstag_14/Uebungen/JsonDb/ch/sbb/it42/jsondb/AdressDatenbankException.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
+ */
+
+package ch.sbb.it42.jsondb;
+
+public class AdressDatenbankException {
+
+
+    //TODO
+    // Aendere diese Klasse so, dass daraus eine RuntimeException wird.
+    //
+    // Speichere den Fehlertext und die urspruengliche Exception in Instanzvariablen, und schreibe
+    // Getter-Methoden, mit der diese Informationen wieder abgerufen werden koennen.
+
+
+    public AdressDatenbankException(String fehlertext, Exception urspruenglicheException) {
+
+    }
+}
---
+++ Kurstag_14/Unterlagen/Streams/ch.sbb.it42.streams/SimpleServer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) Schweizerische Bundesbahnen SBB, 2018.
+ */
+
+package ch.sbb.it42.streams;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SimpleServer {
+
+    public static void main(String[] args) throws Exception {
+        ServerSocket serverSocket = new ServerSocket(7777);
+        System.out.println("Server wartet auf Verbindung auf Port 7777...");
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Client hat Verbindung aufgebaut.");
+            try {
+
+                InputStream inputStream = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                while (true) {
+                    String zeile = reader.readLine();
+                    if (zeile == null) {
+                        break;
+                    }
+                    System.out.println("Habe Text empfangen: " + zeile);
+                }
+            } catch (IOException e) {
+                System.out.println("Ein Fehler ist aufgetreten: " + e);
+            } finally {
+                socket.close();
+            }
+        }
+    }
+}
